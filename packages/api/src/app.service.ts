ae21feda666b441b057a3ff48a982bbe67090252
Jae Lee <jaequery@gmail.com>
2022-10-09 21:23:28 -0700

    chore: hello world

--- packages/api/src/app.service.ts
+++ packages/api/src/app.service.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from './modules/config/config.service';
 
 @Injectable()
 export class AppService {
-  private helloMessage: string;
-
-  constructor(private configService: ConfigService) {
-    this.helloMessage = this.configService.get('HELLO_MESSAGE');
-  }
+  constructor() {}
 
   getHello(): string {
-    return this.helloMessage;
+    return 'Hello world';
   }
 }
